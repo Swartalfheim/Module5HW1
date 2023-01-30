cd011c5bce65bef582ebf073fe5a0f8e74184c89
Denys <86122979+Swartalfheim@users.noreply.github.com>
2023-01-30 11:36:21 +0200

    En

---
+++ M5HW1/Dtos/SupportDto.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace ALevelSample.Dtos;
+
+public class SupportDto
+{
+    [JsonProperty("url")]
+    public string Url { get; set; } = null!;
+
+    [JsonProperty("text")]
+    public string Text { get; set; } = null!;
+}
---
+++ M5HW1/Services/Abstractions/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using ALevelSample.Model;
+
+namespace ALevelSample.Services.Abstractions;
+public interface IUserService
+{
+    Task<User> GetUser(int id);
+    Task<CollectionData<User>> GetUsersByPage(int page);
+    Task<CollectionData<User>> GetUsersByDelay(int delay);
+    Task<Employee> CreateUserEmployee(string name, string job);
+    Task<Employee> UpdateUserEmployee(int id, string name, string job);
+    Task<Employee> ModifyUserEmployee(int id, string name, string job);
+    Task<VoidResult> RemoveUserEmployee(int id);
+}
